89293604a36616bf739d5ba925c2f3d201a5e3d7
Roman Zubko <skcorpoma@gmail.com>
2020-04-17 15:04:08 +0300

    Updates

---
+++ precache-manifest.25666f016703d814030d466e97c72200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc98de30731dc51c67dddb0d315a0646",
+    "url": "/hooks-tsx-react-app/index.html"
+  },
+  {
+    "revision": "b49ec6ccf5bbf0d6b70c",
+    "url": "/hooks-tsx-react-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "32ab0c2bf743b0ac54e9",
+    "url": "/hooks-tsx-react-app/static/js/2.4279faa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-tsx-react-app/static/js/2.4279faa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49ec6ccf5bbf0d6b70c",
+    "url": "/hooks-tsx-react-app/static/js/main.c5f14878.chunk.js"
+  },
+  {
+    "revision": "cba18bd1729035dc145d",
+    "url": "/hooks-tsx-react-app/static/js/runtime-main.ce470653.js"
+  }
+]);
